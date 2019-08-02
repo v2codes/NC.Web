4755137c2607261401f3c88833c8156fc18fc15e
leoguo08 <hao_guo@qq.com>
2019-08-03 01:22:18 +0800

    add Migration

--- NC.Core/Database/CTX.cs
+++ NC.Core/Database/CTX.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using NC.Core.Entities;
+using NC.Core.Helper;
 
 namespace NC.Core.Database
 {
@@ -19,6 +20,24 @@ namespace NC.Core.Database
                : base(options)
         {
 
+        }
+
+        /// <summary>
+        /// 动态添加实体模型
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //取基类=EntityBase 
+            var modelTypes = ReflectionHelper.GetTypesByAssemblyName("NC.Model");
+            foreach (var item in modelTypes)
+            {
+                if (item.BaseType == typeof(Entity<>))
+                {
+                    modelBuilder.Model.GetOrAddEntityType(item);
+                }
+            }
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<T> GetDbSet<T>() where T : class
---
+++ NC.Core/Database/CTXFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace NC.Core.Database
+{
+    /// <summary>
+    /// Create db context instance
+    /// </summary>
+    public class CTXFactory : IDesignTimeDbContextFactory<CTX>
+    {
+        /// <summary>
+        /// create
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public CTX CreateDbContext(string[] args)
+        {
+            var dbContext = new CTX(new DbContextOptionsBuilder<CTX>().UseSqlServer(
+                 new ConfigurationBuilder()
+                   .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), $"appsettings.json"))
+                   .Build()
+                   .GetConnectionString("DefaultConnection")
+               ).Options);
+            dbContext.Database.Migrate();
+            return dbContext;
+        }
+    }
+}
