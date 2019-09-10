d2364844436781121c5535c223388c6c15ace3ad
leo.guo <leo.guo@aisushi.com.cn>
2019-09-10 18:25:08 +0800

    logger dev

---
+++ NC.Common/Logger/LoggerFileProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace NC.Common.log
+{
+    /// <summary>
+    /// TODO 自定义文件日志
+    /// </summary>
+    public class LoggerFileProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NC.Common/Response/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NC.Common.Response
+{
+    class Class1
+    {
+    }
+}
